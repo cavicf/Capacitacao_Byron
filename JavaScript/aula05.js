771f3368c35b584409b65876918d9ff447e19ac8
Camily <cavicfina@gmail.com>
2025-06-09 09:44:16 -0300

    loops

---
+++ JavaScript/aula05.js
@@ -0,0 +1,22 @@
+//Loopings
+
+let i = 0;
+while(i < 10){
+    console.log(i);
+    i++;
+}
+
+console.log('\n')
+
+for(let j = 0; j <= 10; j++){
+    console.log(j);
+}
+
+console.log('\n')
+
+let k = 0;
+do{
+    console.log(k);
+    k++;
+}
+while(k<10)
