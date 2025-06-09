758b3109bc6e617feee2d4f247594d562b97679b
Camily <cavicfina@gmail.com>
2025-06-09 11:14:42 -0300

    Objetos com Vite

---
+++ JavaScript/aula06.js/aulaVite/src/main.js
@@ -0,0 +1,21 @@
+//Objetos
+let user = {
+  nome: 'José',
+  idade: 20,
+  telefone: '9999999',
+  irmaos: [
+    {
+      nome: 'João',
+      idade: 16
+    },
+    {
+      nome:'Pedro',
+      idade: 12
+    }
+  ]
+}
+
+
+document.body.innerText = JSON.stringify(user);
+document.body.innerText = (user.nome);
+document.body.innerText = (user.irmaos[0].nome);
