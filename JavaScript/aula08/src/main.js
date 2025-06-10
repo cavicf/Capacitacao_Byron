0c982ad8b0ba1aa43d7dcbf92af324cfbd67a358
Camily <cavicfina@gmail.com>
2025-06-10 08:23:56 -0300

    strings

---
+++ JavaScript/aula08/src/main.js
@@ -0,0 +1,10 @@
+let numero = 6;
+let frase = 'A idade do meu irmão é: ';
+let fraseFinal = frase + numero; //concatena
+console.log(fraseFinal);
+
+let fraseFim = `A idade do meu irmão é: ${numero}`;
+console.log(fraseFim);
+document.body.innerText = frase;
+
+
