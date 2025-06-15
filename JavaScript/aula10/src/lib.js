259a94281240feccf03f9e3b167e54db35fd6ef4
Camily <cavicfina@gmail.com>
2025-06-15 18:59:29 -0300

    import e export

---
+++ JavaScript/aula10/src/lib.js
@@ -0,0 +1,11 @@
+const soma = (a, b) => {
+    return a + b;
+}
+
+const sub = (a, b) => {
+    return a - b;
+}
+
+const pi2 = 3.12**2;
+
+export {soma, sub, pi2};
---
+++ JavaScript/aula10/src/main.js
@@ -0,0 +1,3 @@
+import {soma, sub, pi2} from './lib';
+
+document.body.innerText = soma(pi2, 2);
