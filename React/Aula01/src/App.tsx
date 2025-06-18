fe4e1a66d4af135570ec45e7da502223a8978bb6
Camily <cavicfina@gmail.com>
2025-06-18 14:56:35 -0300

    Introdução ao React

---
+++ React/Aula01/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Botao from './components/Botao'
+
+function App() {
+  return (
+      <div>
+        <h1>Hello World</h1>
+        <Botao title="botao 1"></Botao>
+        <Botao title ="botao 2"></Botao>
+      </div>
+  )
+}
+
+export default App
---
+++ React/Aula01/src/components/Botao.tsx
@@ -0,0 +1,7 @@
+export default function Botao(props: any) {
+    return (
+        <button>
+            {props.title}
+        </button>
+    )
+}
